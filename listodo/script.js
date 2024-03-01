699f1ea28d3f67c5a4f51770030d7f7b56757b5f
Jedi992 <139563219+Jedi992@users.noreply.github.com>
2024-03-01 16:30:02 +0300

    Add files via upload

---
+++ listodo/script.js
@@ -0,0 +1,49 @@
+`use strict`
+
+const button = document.querySelector('.btn');
+const input = document.querySelector(".input__todo")
+const ul = document.querySelector(".ul")
+const t = document.querySelector(".element")
+const span = t.content.querySelector('.task')
+const btnDelete = document.querySelector(".btn_delete")
+let task = []
+let taskCheckBox = document.querySelector(".list_task")
+
+
+// function enterNum() {
+//     console.log("test");
+// }
+function addTaskForEnter (e) {
+    if (e.keyCode === 13) {
+        document.querySelector(".btn").click()
+    }
+}
+function addTask() {
+    task.push(
+        {id: task.length + 1 , flagComplete:true, text:input.value}
+    )
+    if (input.value.length <= 3) {
+        return
+    }
+    task.forEach((elem) => {
+        span.textContent = elem.text
+    })
+    // console.log(task)
+    let list = t.content.cloneNode(true);
+    ul.append(list)
+        input.value = ''
+
+}
+
+function clearFunc (event) {
+    event.target.closest('.list_task').remove()
+}
+
+function taskComplete(event) {
+    if (event.target.closest('.list_task').classList.contains("checkBoxComplete") === false) {
+    event.target.closest('.list_task').classList.add("checkBoxComplete");
+    } else {
+        event.target.closest('.list_task').classList.remove('checkBoxComplete')
+    }
+}
+
